20527e70f128ab96e76a13b58a8aeea36a89c6a7
Luken <lukenlarragithub@gmail.com>
2026-03-13 11:25:47 +0100

    Agregar manejo de entorno virtual en los servicios de CCX para mejorar la ejecución de pruebas

--- features/steps/ccx_data_engineering_service.py
+++ features/steps/ccx_data_engineering_service.py
@@ -18,6 +18,7 @@
 import os
 import subprocess
 import time
+import sys
 
 import requests
 from behave import given, when
@@ -42,6 +43,9 @@ def start_ccx_upgrades_data_eng(context, port):
     for row in context.table:
         var, val = row["variable"], row["value"]
         env[var] = val
+
+    venv_bin = os.path.dirname(sys.executable)
+    env["PATH"] = f"{venv_bin}{os.pathsep}{env.get('PATH', '')}"
 
     stdout_path = path_from_context(context, "ccx-upgrades-data-eng", "stdout")
     stderr_path = path_from_context(context, "ccx-upgrades-data-eng", "stderr")
