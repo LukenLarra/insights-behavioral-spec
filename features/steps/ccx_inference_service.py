259843f14eb36546c48344ab065a9168c1266fcc
Luken <lukenlarragithub@gmail.com>
2026-03-16 12:42:16 +0100

    Agregar manejo del entorno virtual al iniciar los servicios de CCX Data Engineering e Inference

--- features/steps/ccx_inference_service.py
+++ features/steps/ccx_inference_service.py
@@ -17,6 +17,7 @@
 import os
 import subprocess
 import time
+import sys
 
 from behave import given, when
 from common_http import check_service_started
@@ -61,7 +62,10 @@ def start_ccx_inference_mock_service(context, port):
     context.add_cleanup(stdout_file.close)
     context.add_cleanup(stderr_file.close)
 
-    popen = subprocess.Popen(params, stdout=stdout_file, stderr=stderr_file)
+    env = os.environ.copy()
+    venv_bin = os.path.dirname(sys.executable)
+    env["PATH"] = f"{venv_bin}{os.pathsep}{env.get('PATH', '')}"
+    popen = subprocess.Popen(params, stdout=stdout_file, stderr=stderr_file, env=env)
     assert popen is not None
     check_service_started(context, "localhost", port, attempts=10, seconds_between_attempts=1)
 
